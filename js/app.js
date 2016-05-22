c5728ee9fe8c85493eecd53cb1f3ac325f7f103b
cajoue <cajoue@users.noreply.github.com>
2016-05-22 13:41:22 +0200

    feat: create catList with data for multiple cats

--- js/app.js
+++ js/app.js
@@ -2,6 +2,47 @@
 // move all cat logic to it's own function
 // get ready for more cats
 
+// bring on the cat data
+// alternative this data could be loaded from a server (apparently trivial to implement)
+var initialCats = [
+  {
+    clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    imgAttribution: 'https://flickr.com/photos/bigtallguy/434164568',
+    nicknames: ['kit', 'kitty', 'kitkat', 'kitty kat']
+  },
+  {
+    clickCount: 0,
+    name: 'Tiger',
+    imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+    imgAttribution: 'https://flickr.com/photos/xshamx/4154543904',
+    nicknames: ['tigger']
+  },
+  {
+    clickCount: 0,
+    name: 'Scaredy',
+    imgSrc: 'img/22252709_010df3379e_z.jpg',
+    imgAttribution: 'https://flickr.com/photos/kpjas/22252709',
+    nicknames: ['casper']
+  },
+  {
+    clickCount: 0,
+    name: 'Shadow',
+    imgSrc: 'img/1413379559_412a540d29_z.jpg',
+    imgAttribution: 'https://flickr.com/photos/malfet/1413379559',
+    nicknames: ['shooby']
+  },
+  {
+    clickCount: 0,
+    name: 'Sleepy',
+    imgSrc: 'img/9648464288_2516b35537_z.jpg',
+    imgAttribution: 'https://flickr.com/photos/onesharp/9648464288',
+    nicknames: ['zzzzzz']
+  }
+]
+
+
 // data is an object literal that contains cat data
 var Cat = function (data){
   // set values based on the object literal data that gets passed in
@@ -29,15 +70,20 @@ var Cat = function (data){
 var ViewModel = function (){
   // store a pointer to hold reference to the ViewModel scope
   var self = this;
+
+  // an observableArray to store all cats
+  this.catList = ko.observableArray([]);
+
+  // make the cats
   // pass object literal data to new Cat
-  // set data where Cat is created
-  this.currentCat = ko.observable(new Cat({
-    clickCount: 0,
-    name: 'Tabby',
-    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-    imgAttribution: 'https://flickr.com/photos/big',
-    nicknames: ['kit', 'kitty', 'kitkat', 'kitty kat']
-  }));
+  // loop over the data array and push each new cat into catList
+  // use 'self' to avoid 'this' keyword scope confusion
+  initialCats.forEach(function(catItem){
+    self.catList.push( new Cat(catItem) );
+  });
+
+  // use first cat in catList as the first screen cat
+  this.currentCat = ko.observable(this.catList()[0]);
 
   // update increment counter to get click info from the Cat Model
   // use 'self' inside the function instead of 'this':
